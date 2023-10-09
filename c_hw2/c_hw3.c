e23b1ed59945f621b1b9b6141288ac5a92c5f211
Mue <Mue@mue.com>
2023-10-10 01:52:46 +0300

    exercise 02 fixed

--- c_hw2/c_hw3.c
+++ c_hw2/c_hw3.c
@@ -6,12 +6,16 @@
 
 int ft_str_is_alpha(char *str)
 {
+    if(str == NULL)
+    {
+    return 1;
+    }
     int i;
     i = 0;
      
 while(str[i])
 {
-    if(str[i] >= 'A' || str[i] <= 'Z' || str[i] >= 'a' || str[i] <= 'z')
+    if(str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
     {
         i++;
     }
